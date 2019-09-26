aa8f38f2df0b84f6e032707aeab67bcefd02cc0e
L A R K <lark.buckingham@sjsu.edu>
2019-09-26 15:35:27 -0700

    created new branch for demo

--- randomizerDemo/sketchPullingRandomWHomeAndEnd.js
+++ randomizerDemo/sketchPullingRandomWHomeAndEnd.js
@@ -20,7 +20,6 @@ let dogs = [{
 }];
 
 let randomIndex;
-let animating = false;
 
 function setup() {
   createCanvas(600, 600);
@@ -28,35 +27,23 @@ function setup() {
   textSize(32);
 
   text("click to randomize", 50, 50);
+
 }
 
 function draw() {
 
-  if (animating == true){
-    ellipse(random(width), random(height), random(50, 200));
-
-  }
 }
 
-function randomizer(){
-  animating = false;
+function mousePressed() {
 
   if (dogs[0]) {
     // this displays random name and splices it out of array
     background(random(200, 255));
     randomIndex = int(random(dogs.length));
-    text(`${dogs[randomIndex].name}'s favorite color inspect
-     ${dogs[randomIndex].color}`, 50, 50);
-    // text(dogs[randomIndex].name + "'s favorute color is " + dogs[randomIndex].color, 50, 50);
+    text(dogs[randomIndex].name, 50, 50);
     dogs.splice(randomIndex, 1);
   } else {
     background(random(200, 255));
     text("nothing left!", 50, 50);
   }
-}
-
-function mousePressed() {
-  animating = true;
-  setTimeout(randomizer, 2000);
-
 }
