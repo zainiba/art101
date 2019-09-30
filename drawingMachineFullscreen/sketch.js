9ed1559c91d4265b4f757c9d6cfb14826eda9247
L A R K <lark.buckingham@sjsu.edu>
2019-09-30 07:57:41 -0700

    added fullscreen drawing machine

--- drawingMachineFullscreen/sketch.js
+++ drawingMachineFullscreen/sketch.js
@@ -1,21 +1,27 @@
-let array = [];
+
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  createCanvas(600, 600);
-  background(200);
+  createCanvas(windowWidth, windowHeight);
+  background(220, 50, 133);
 
-  strokeWeight(5);
-  noFill();
+
+
 
 }
 
 function draw() {
+  background(220, 50, 133, 5);
+  strokeWeight(strokeWidth);
 
-  if (mouseIsPressed) {
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
-    array.push([mouseX, mouseY]);
-  }
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
+
+
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
 
 }
 
@@ -24,16 +30,9 @@ function keyTyped() {
   if (key === 's') {
     // save this image
     saveCanvas('fileName', 'png');
-  } else if (key === 'd') {
-    // display image
-    background(255);
-
-    beginShape();
-    for (let i = 0; i < array.length; i++) {
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1])
-    }
-    endShape();
+  } else if (key === 'c') {
+    // clear the image
+    clear();
 
   }
 
