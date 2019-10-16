fffc6fb7d8b82d26cd325fecf97b9a21d1c1b8f6
L A R K <lark.buckingham@sjsu.edu>
2019-10-16 11:35:09 -0700

    reset randomizer to where vid tutorials left off

--- assignments/randomizerDemo/sketch.js
+++ assignments/randomizerDemo/sketch.js
@@ -20,22 +20,41 @@ let dogs = [{
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
+  textSize(24);
+
+  text("click to randomize", 50, 50);
 
 }
 
 function draw() {
 
+  if(animating == true){
+    ellipse(random(width), random(height), random(50, 200));
+  }
 }
 
-function mousePressed() {
+function randomizer() {
+  animating = false;
+  if (dogs[0]) {
+    // this displays random and splices it out of array
+    background(random(200, 255));
+    randomIndex = int(random(dogs.length));
+    text(dogs[randomIndex].name, 50, 50);
+    dogs.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("nothing left!", 50, 50);
+  }
+}
 
-  // this displays random name and splices it out of array
-  background(random(200, 255));
-  randomIndex = int(random(dogs.length));
-  text(dogs[randomIndex].name, 50, 50);
-  dogs.splice(randomIndex, 1);
+
+
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
 }
