a382ca7409a0e49a36ef9027ddb442101bbb3c97
L A R K <lark.buckingham@sjsu.edu>
2019-10-17 09:55:37 -0700

    added trolls

--- assignments/randomizerDemo/sketch.js
+++ assignments/randomizerDemo/sketch.js
@@ -21,34 +21,63 @@ let dogs = [{
 
 let randomIndex;
 let animating = false;
+let trolls = [];
+let imageCounter = 0;
+
+function preload() {
+
+  for (let i = 0; i <= 15; i++) {
+    trolls[i] = loadImage(`assets/troll_${i}.JPG`)
+  }
+
+}
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
-  textSize(24);
+  background(20, 40, 200);
+  textSize(36);
+  textFont('Courier new');
+  textAlign(CENTER);
+  textStyle(BOLD);
+  fill(255);
+  imageMode(CENTER);
+  frameRate(8);
 
-  text("click to randomize", 50, 50);
-
+  text("click to randomize", width/2, height/2);
+  console.log(trolls);
 }
 
 function draw() {
 
-  if(animating == true){
-    ellipse(random(width), random(height), random(50, 200));
+
+  if (animating == true) {
+    clear();
+    image(trolls[imageCounter], width / 2, height / 2);
+
+    if (imageCounter < trolls.length - 1) {
+      imageCounter++;
+      console.log(imageCounter);
+    } else {
+      imageCounter = 0;
+    }
   }
+
 }
 
 function randomizer() {
   animating = false;
   if (dogs[0]) {
     // this displays random and splices it out of array
-    background(random(200, 255));
+    // background(random(200, 255));
+    clear();
     randomIndex = int(random(dogs.length));
-    text(dogs[randomIndex].name, 50, 50);
+
+    image(random(trolls), width / 2, height / 2);
+    text(dogs[randomIndex].name, width / 2, height - 60);
     dogs.splice(randomIndex, 1);
   } else {
-    background(random(200, 255));
-    text("nothing left!", 50, 50);
+    background(20, 40, 200);
+    text("nothing left!", width / 2, height / 2);
   }
 }
 
