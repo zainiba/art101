68db5ef48ede2b7a8c816a38ababcdd0937e79be
L A R K <lark.buckingham@sjsu.edu>
2019-10-24 15:06:22 -0700

    added button sound and visibility demo

--- assignments/DOM-candle/sketch.js
+++ assignments/DOM-candle/sketch.js
@@ -1,7 +1,31 @@
+let lightCandleBtn;
+let candleImage;
+let matchSound;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  matchSound = loadSound('assets/matchStrike.mp3');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+
+  lightCandleBtn = select('#lightCandleButton');
+  candleImage = select('#candle');
+
+  lightCandleBtn.mousePressed(toggleCandle);
 }
 
 function draw() {
   background(220);
+}
+
+function toggleCandle() {
+  matchSound.setVolume(0.1);
+  matchSound.play();
+
+  setTimeout(function() {
+    candleImage.show();
+  }, 1500);
+
 }
